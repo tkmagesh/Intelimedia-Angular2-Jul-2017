e02416b22b4045bc7b4421f6bfb7193c557b6e3a
Magesh <tkmagesh77@gmail.com>
2017-08-02 21:00:20 +0530

    Calculator two - assignment - solution 3

--- hello-world-app/src/app/calculator/calculatorTwo.component.ts
+++ hello-world-app/src/app/calculator/calculatorTwo.component.ts
@@ -14,23 +14,4 @@ export class CalculatorTwoComponent{
 	constructor(){
 		this.model = new CalculatorModel();
 	}
-
-	calculate(){
-		/*switch (this.operator) {
-			case "add":
-				this.model.add();
-				break;
-			case "subtract":
-				this.model.subtract();
-				break;
-			case "multiply":
-				this.model.multiply();
-				break;
-			case "divide":
-				this.model.divide();
-				break;
-			
-		}*/
-		this.model[this.operator]();
-	}
 }
